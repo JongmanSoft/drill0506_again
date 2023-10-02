b0625cc62b4e6e5cc23cf96c9f0b82234c18a695
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-10-02 22:57:47 +0900

    랜덤위치 손출력

--- drill05 again.py
+++ drill05 again.py
@@ -29,14 +29,15 @@ def reset_world():
     running = True
     x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-    hx, hy = TUK_WIDTH -50, TUK_HEIGHT -50
+    #hx, hy = TUK_WIDTH -50, TUK_HEIGHT -50
+    hx, hy = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)
 
 
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx,hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
 
